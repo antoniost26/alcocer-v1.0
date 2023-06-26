431efd0f72c776ac6d304605e1d239597f435e3e
antoniost26 <stanantonio263@gmail.com>
2023-06-27 02:16:03 +0300

    fixed presence error

--- src/events/ready/setpresence.js
+++ src/events/ready/setpresence.js
@@ -1,3 +1,5 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = async (client) => {
     client.user.setPresence({ activities: [{ name: '.gg/anzu', ActivityType: ActivityType.Watching }], status: 'dnd' });
 };
