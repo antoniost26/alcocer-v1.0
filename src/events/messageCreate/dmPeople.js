0a3540ab321d72338177c2fd0be5a3f5c7bd4931
antoniost26 <stanantonio263@gmail.com>
2023-12-23 23:35:21 +0200

    Update dmPeople.js

--- src/events/messageCreate/dmPeople.js
+++ src/events/messageCreate/dmPeople.js
@@ -20,5 +20,5 @@ module.exports = async (client, message) => {
   setTimeout(() => {
     sentMessage.delete().catch((error) => console.log(error));
     message.delete().catch((error) => console.log(error));
-  }, 1000);
+  }, 300);
 };
